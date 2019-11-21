a068fab5fb854d1eebbb5c8398476429476a162a
J3ffJessie <jejess6125@gmail.com>
2019-11-20 21:24:02 -0600

    Added the DB connection to Mongoose.  Also initiated the schema for the posts collection.  List will show DB posts on load.

--- app.js
+++ app.js
@@ -21,14 +21,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+//creating our list item schema
 const itemsSchema = {
   name: String
 };
 
-
+//building the mongoose model for future list items
 const Item = mongoose.model("Item", itemsSchema);
+
 
 const item1 = new Item({
   name: "Welcome to your To-Do List"
@@ -42,57 +47,59 @@ const item3 = new Item({
   name: "<---Hit this button to delete an item."
 });
 
+//array to load on initial start up if the list is completely empty upon loading
+const defaultItems = [item1, item2, item3];
 
-const defaultItems=[item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if(err){
-    console.log(err); 
-  } else {
-    console.log("Successfully Added Entries to DB")
-  }
-});
 
 app.get("/", function (req, res) {
+  //parsing through the Item model and finding the post items
+  Item.find({}, function (err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully Added Entries to DB")
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  });
 
 
+  app.post("/", function (req, res) {
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
 
 
-  res.render("list", {
-    listTitle: "Today",
-    newListItems: items
+  app.get("/work", function (req, res) {
+    res.render("list", {
+      listTitle: "Work List",
+      newListItems: workItems
+    });
+  });
+
+  app.get("/about", function (req, res) {
+    res.render("about");
   });
 
 });
-
-app.post("/", function (req, res) {
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
-
-
-app.get("/work", function (req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
-
-
-
 app.listen(5500, function () {
   console.log("Server has started on Port 5500");
 });
