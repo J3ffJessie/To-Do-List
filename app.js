ef8761cb3108654cd77f310d2f7e54053c0619b0
Jeff Jessie <jejess6125@gmail.com>
2020-11-07 14:21:57 -0600

    Minor style changes and button location change

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.get("/", function (req, res) {
 
 let port = process.env.PORT;
 if(port == null || port == ""){
-  port=8000;
+  port=5000;
 }
 app.listen(port, function(){
   console.log("Server has started successfully");
